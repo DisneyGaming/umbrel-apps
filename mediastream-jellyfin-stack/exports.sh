0f2f7fb582fb67bdb2878651346db2593dd6d000
DisneyGaming <63114140+DisneyGaming@users.noreply.github.com>
2026-01-22 02:34:30 +0000

    test

---
+++ mediastream-jellyfin-stack/exports.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Export Zurg RClone mount path for other apps
+export APP_MEDIASTREAM_JELLYFIN_STACK_RCLONE_MOUNT_PATH="/mnt/zurg"
+
+# Export Jellyfin API port
+export APP_MEDIASTREAM_JELLYFIN_STACK_JELLYFIN_PORT="8096"
